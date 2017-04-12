6b35296b0b414b2e04af9f849b19d70655f6f6f0
trdmm <trdmm@topplayer.win>
2017-04-12 18:22:34 +0800

    重新进入设置完成
    重新进入设置完成

--- src/main/java/com/sat/mobilesafe/Activities/SetupOverActivity.java
+++ src/main/java/com/sat/mobilesafe/Activities/SetupOverActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.sat.mobilesafe.R;
@@ -20,10 +21,27 @@ public class SetupOverActivity extends AppCompatActivity{
         boolean setup_over = SpUtils.getBoolean(this, ConstantValue.SETUP_OVER, false);
         if (setup_over){
             setContentView(R.layout.activity_setup_over);
+            initUI();
         }else {
             Intent intent = new Intent(this, Setup1Activity.class);
             startActivity(intent);
             finish();
         }
+    }
+
+    private void initUI() {
+        String num = SpUtils.getString(getApplicationContext(), ConstantValue.CONTACT_NUM, "");
+        TextView tv_safe_num = (TextView) findViewById(R.id.tv_safe_num);
+        TextView re_set = (TextView) findViewById(R.id.re_set);
+
+        tv_safe_num.setText(num);
+        re_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Setup1Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
